44a2fe827c959426fe84c6516c7d76693045df6b
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-11 14:58:05 +0200

    World Generation and movement

---
+++ HuubdeKubus_Prototype/Assets/Scripts/WorldGeneration.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour {
+
+    public GameObject tablePrefab;
+    GameObject playerObj;
+    private bool triggered;
+    
+
+	// Use this for initialization
+	void Start () {
+        playerObj = GameObject.Find("PlayerSphere");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] tables = GameObject.FindGameObjectsWithTag("Tables");
+        foreach (GameObject table in tables)
+        {
+            if(table.transform.position.z > 64)
+            {
+                Destroy(table);
+            }
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.transform.position.z);
+        if (collision.gameObject.name == playerObj.name)
+        {
+            var worldBlock = (GameObject)Instantiate(tablePrefab, new Vector3(0,0,-46.6f), Quaternion.identity);
+            worldBlock.transform.parent = GameObject.Find("GameWorld").transform;
+            Destroy(GetComponent<WorldGeneration>());
+        }
+    }
+}
---
+++ HuubdeKubus_Prototype/Assets/Scripts/WorldMover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMover : MonoBehaviour {
+    GameObject theWorld;
+
+	// Use this for initialization
+	void Start () {
+        theWorld = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        theWorld.transform.Translate(new Vector3(0,0,0.3f));
+	}
+}
