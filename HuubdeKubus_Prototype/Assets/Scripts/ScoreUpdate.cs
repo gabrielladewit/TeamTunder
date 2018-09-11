75641fc91b4ad9b6ce8ddd3db3d7ee29f8f75a14
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-11 15:33:33 +0200

    Added Score to scene

---
+++ HuubdeKubus_Prototype/Assets/Scripts/ScoreUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUpdate : MonoBehaviour {
+
+    GameObject theScore, playerObj, topObj;
+
+	// Use this for initialization
+	void Start () {
+        theScore = GameObject.Find("Score");
+        playerObj = GameObject.Find("PlayerSphere");
+        topObj = GameObject.Find("Top");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var score = (int) Vector3.Distance(playerObj.transform.position, topObj.transform.position);
+        theScore.GetComponent<Text>().text = score.ToString();
+
+    }
+}
