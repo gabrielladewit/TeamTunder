a93c669171e8b6c5c9e7621c49f50150943f98a9
Martijn Koekkoek <martini4k@live.nl>
2018-09-12 22:36:54 +0200

    Added bullets

--- HuubdeKubus_Prototype/Assets/Scripts/ObstacleSpawner.cs
+++ HuubdeKubus_Prototype/Assets/Scripts/ObstacleSpawner.cs
@@ -18,8 +18,16 @@ public class ObstacleSpawner : MonoBehaviour
     private Vector3 upRotStart = new Vector3(0,2,22);
     private Vector3 downRotStart = new Vector3(0,2,-22);
 
+    GameObject gunV1, gunV2, gunV3, gunH1, gunH2, gunH3;
+    private float instantiateTimer = 6;
+    public GameObject o_bullet;
+    bool shoot = true;
+    float timeShoot = 1f;
+    int end = 0;
 
-    float countDown = 10f;
+    public GameObject floor;
+
+    float countDown = 12f;
     public int speed = 6;
     public int rotSpeed = 12;
 
@@ -30,11 +38,16 @@ public class ObstacleSpawner : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        gunV1 = new GameObject("V1");
+        gunV2 = new GameObject("V2");
+        gunV3 = new GameObject("V3");
+        gunH1 = new GameObject("H1");
+        gunH2 = new GameObject("H2");
+        gunH3 = new GameObject("H3");
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         switch (scene)
         {
@@ -48,6 +61,10 @@ public class ObstacleSpawner : MonoBehaviour
                 Turn();
                 break;
             case 3:
+                Bullet();
+                break;
+            case 4:
+                MovingWall();
                 break;
         }
     }
@@ -162,6 +179,119 @@ public class ObstacleSpawner : MonoBehaviour
                 }
             }
         }
+    }
+
+    public void Bullet()
+    {
+        float PosX = floor.transform.position.x;
+        float PosY = floor.transform.position.z;
+
+        float SizeX = floor.transform.localScale.x;
+        float SizeY = floor.transform.localScale.z;
+
+        float bulletHeight = 1.5f;
+
+        float speedBullet = 250;
+
+        if (start)
+        {
+            gunV1.transform.position = new Vector3(PosX - ((SizeX / 2) / 2), bulletHeight, PosY + (SizeY / 2));
+            gunV2.transform.position = new Vector3(PosX, bulletHeight, PosY - (SizeY / 2));
+            gunV3.transform.position = new Vector3(PosX + ((SizeX / 2) / 2), bulletHeight, PosY + (SizeY / 2));
+
+            gunH1.transform.position = new Vector3(PosX - (SizeX / 2), bulletHeight, PosY + ((SizeY / 2) / 2));
+            gunH2.transform.position = new Vector3(PosX + (SizeX / 2), bulletHeight, PosY);
+            gunH3.transform.position = new Vector3(PosX - (SizeX / 2), bulletHeight, PosY - ((SizeY / 2) / 2));
+
+            end = 0;
+            start = false;
+        }
+
+
+        if (!start)
+        {
+            instantiateTimer -= Time.deltaTime;
+
+            GameObject clone;
+
+            if (!shoot)
+            {
+                timeShoot -= Time.deltaTime;
+                if (timeShoot <= 0f)
+                {
+                    shoot = true;
+                }
+
+            }
+            switch ((int)instantiateTimer)
+            {
+                case 6:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunV1.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.back * speedBullet);
+                        timeShoot = 1f;
+                        shoot = false;
+                    }
+                    break;
+                case 5:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunH2.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.left * speedBullet);
+                        timeShoot = 1f;
+                        shoot = false;
+                    }
+                    break;
+                case 4:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunH3.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.right * speedBullet);
+                        timeShoot = 1f;
+                        shoot = false;
+                    }
+                    break;
+                case 3:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunV2.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.forward * speedBullet);
+                        timeShoot = 1f;
+                        shoot = false;
+                    }
+                    break;
+                case 2:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunV3.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.back * speedBullet);
+                        timeShoot = 1f;
+                        shoot = false;
+                    }
+                    break;
+                case 1:
+                    if (shoot)
+                    {
+                        clone = Instantiate(o_bullet, gunH1.transform.position, transform.rotation);
+                        clone.GetComponent<Rigidbody>().AddForce(Vector3.right * speedBullet);
+                        timeShoot = 1f;
+                        instantiateTimer = 7;
+                        end++;
+                        shoot = false;
+                    }
+                    break;
+            }
+            if (end >= 3)
+            {
+                scene++;
+                start = true;
+            }
+        }
+    }
+
+    public void MovingWall() {
+
     }
 }
 
