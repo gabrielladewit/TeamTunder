e8515e971c5dddbc7dc018082cce85fbe9cdacf1
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-18 09:26:23 +0200

    added "calibration"

---
+++ HuubdeKubus_Prototype/Assets/Scripts/GyroCalibrator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GyroCalibrator : MonoBehaviour {
+
+    private Stopwatch stopwatch;
+    private int updates = 0;
+    private Vector3 totalInput;
+    public Vector3 averageInput;
+    public Quaternion gyroAttitude;
+    private GameObject calibObj;
+
+    // Use this for initialization
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        stopwatch = new Stopwatch();
+        totalInput = new Vector3(0, 0, 0);
+    }
+
+    public void startstab()
+    {
+        updates = 0;
+        totalInput = new Vector3(0, 0, 0);
+        stopwatch.Start();
+        StartCoroutine(Calibrate());
+    }
+
+    IEnumerator Calibrate()
+    {
+        while (stopwatch.ElapsedMilliseconds < 3000)
+        {
+            updates++;
+            totalInput.x += Input.acceleration.x;
+            totalInput.y += Input.acceleration.y;
+            totalInput.z += Input.acceleration.z;
+            UnityEngine.Debug.Log("Added intel");
+            yield return new WaitForSeconds(0.2f);
+        }
+        stopwatch.Stop();
+        stopwatch.Reset();
+        gyroAttitude = Input.gyro.attitude;
+        averageInput = (totalInput / updates);
+        StopCoroutine("Calibrate");
+    }
+}
--- HuubdeKubus_Prototype/Assets/Scripts/GyroTest.cs
+++ HuubdeKubus_Prototype/Assets/Scripts/GyroTest.cs
@@ -7,21 +7,26 @@ public class GyroTest : MonoBehaviour {
     public bool isFlat = true;
     private Rigidbody rigid;
     Vector3 accel;
+    private GyroCalibrator gyroCalib;
 
     // Use this for initialization
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
         accel = new Vector3(0,0,0);
+        gyroCalib = GameObject.Find("Stored").GetComponent<GyroCalibrator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 tilt = Input.acceleration;
-
+        
         if (isFlat)
-            tilt = Quaternion.Euler(90, 0, 0) * tilt * Time.deltaTime * 2000;
+            tilt = Quaternion.Euler(45, 0, 0) * tilt * Time.deltaTime * 100;
+        tilt.x = Mathf.Ceil(tilt.x);
+        tilt.y = Mathf.Ceil(tilt.y);
+        tilt.z = Mathf.Ceil(tilt.z);
 
         rigid.AddForce(tilt);
     }
