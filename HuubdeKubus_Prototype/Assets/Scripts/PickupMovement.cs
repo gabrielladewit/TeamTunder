43e6cf2c2c8638ef6991e3932e96e4d2d9d0793c
dev-Chris <chrisbloem1@hotmail.com>
2018-09-18 10:50:08 +0200

    Pickup hoover added

---
+++ HuubdeKubus_Prototype/Assets/Scripts/PickupMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupMovement : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float verticalSpeed = 1;
+    public float amplitude = 0.5f;
+    private Vector3 tempPosition;
+    // Use this for initialization
+    void Start()
+    {
+        tempPosition = transform.position;
+        Debug.Log("x: " + tempPosition.x + " y: " + tempPosition.y + " z: " + tempPosition.z);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        tempPosition.x += horizontalSpeed;
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude + 4;
+        transform.position = tempPosition;
+
+        Debug.Log("x: " + tempPosition.x + " y: " + tempPosition.y + " z: " + tempPosition.z);
+        Debug.Log("/////////////////////////////");
+        Debug.Log("x: " + transform.position.x + " y: " + transform.position.y + " z: " + transform.position.z);
+    }
+}
