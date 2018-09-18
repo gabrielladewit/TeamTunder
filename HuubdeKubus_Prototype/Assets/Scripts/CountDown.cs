1a1f149c5da102afd39249be0df0d9daa023e586
Martijn Koekkoek <martini4k@live.nl>
2018-09-18 13:12:52 +0200

    added new prefabPuzzle example, breakable object

---
+++ HuubdeKubus_Prototype/Assets/Scripts/CountDown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour {
+
+    public float time = 3f;
+
+	// Update is called once per frame
+	void Update () {
+        time -= Time.deltaTime;
+
+        if (time <= 0f)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
