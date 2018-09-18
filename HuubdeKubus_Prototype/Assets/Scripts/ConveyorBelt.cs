98e4dc309f02768dfd6449e789a69701aee4687b
gabrielladewit <gabrielladewit@hotmail.com>
2018-09-18 13:34:10 +0200

    Conveyor belt

---
+++ HuubdeKubus_Prototype/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour {
+
+    public float beltSpeed;
+
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.attachedRigidbody.AddForce(beltSpeed, 0, 0);
+    }
+}
