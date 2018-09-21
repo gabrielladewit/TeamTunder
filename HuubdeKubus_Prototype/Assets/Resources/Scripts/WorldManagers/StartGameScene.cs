84ae0523683302db10fac1423c02d78fef32ebc0
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-21 14:35:49 +0200

    Changed the Menu, dearthscreen and settings screen Changed screen resolution to scale to phone size

---
+++ HuubdeKubus_Prototype/Assets/Resources/Scripts/WorldManagers/StartGameScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameScene : MonoBehaviour {
+
+    GameObject pausePanel, deathPanel;
+
+	// Use this for initialization
+	void Start () {
+        GameObject pPanel = GameObject.Find("PausePanel");
+        if (pPanel != null)
+        {
+            pPanel.SetActive(false);
+        }
+
+        GameObject dPanel = GameObject.Find("DeathPanel");
+        if (dPanel != null)
+        {
+            dPanel.SetActive(false);
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
