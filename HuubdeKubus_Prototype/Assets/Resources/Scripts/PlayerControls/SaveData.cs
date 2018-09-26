3a0fe3c516c35d75a4c675313e1c066c46f72bf8
Luc Schuttel <luc_schuttel@hotmail.com>
2018-09-26 11:31:42 +0200

    working on saving/loading

---
+++ HuubdeKubus_Prototype/Assets/Resources/Scripts/PlayerControls/SaveData.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class SaveData
+{
+
+    public static InventoryContainer invContainer = new InventoryContainer();
+
+    public delegate void SerializeAction();
+    public static event SerializeAction OnLoaded;
+    public static event SerializeAction OnBeforeSave;
+
+    public static void Load(string path)
+    {
+        invContainer = LoadInventory(path);
+
+        foreach (InventoryData data in invContainer.inventories)
+        {
+
+        }
+    }
+
+    public static void Save(string path, InventoryContainer container)
+    {
+
+    }
+
+    public static void AddInventoryData(InventoryData data)
+    {
+        invContainer.inventories.Add(data);
+    }
+
+    public static void ClearInventory()
+    {
+        invContainer.inventories.Clear();
+    }
+
+    private static InventoryContainer LoadInventory(string path)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(InventoryContainer));
+
+        FileStream stream = new FileStream(path, FileMode.Truncate);
+
+        InventoryContainer container = serializer.Deserialize(stream) as InventoryContainer;
+
+        stream.Close();
+
+        return container;
+    }
+
+    private static void SaveInventory(string path, InventoryContainer container)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(InventoryContainer));
+
+        FileStream stream = new FileStream(path, FileMode.Truncate);
+
+        serializer.Serialize(stream, container);
+
+        stream.Close();
+    }
+}
---
+++ HuubdeKubus_Prototype/Assets/Resources/Scripts/WorldManagers/SaveController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveController : MonoBehaviour {
+
+    public const string inventoryPath = "prefabs/inventory";
+    private static string dataPath = string.Empty;
+
+	void Awake () {
+        if (Application.platform)
+        {
+            dataPath = System.IO.Path.Combine(Application.dataPath, "Resources/inventory.xml");
+        }
+	}
+	
+	public static CreateInventory(string path, int money, int lives, bool boughtHeavy)
+    {
+        GameObject prefab = Resources.Load<GameObject>(path);
+
+        GameObject x = GameObject.Instantiate(prefab, );
+
+        GameObject inventory;
+
+        return inventory;
+    }
+}
