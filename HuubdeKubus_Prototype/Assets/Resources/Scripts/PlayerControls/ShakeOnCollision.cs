d4587845ddca48782d94315aeb621da217b364f7
Luc Schuttel <luc_schuttel@hotmail.com>
2018-10-14 17:47:21 +0200

    Added camera shake to both versions

---
+++ HuubdeKubus_Prototype/Assets/Resources/Scripts/PlayerControls/ShakeOnCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class ShakeOnCollision : MonoBehaviour {
+
+    public float Magnitude = 2f;
+    public float Roughness = 10f;
+    public float FadeOutTime = 5f;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        CameraShaker.Instance.ShakeOnce(Magnitude, Roughness, 0, FadeOutTime);
+    }
+
+}
