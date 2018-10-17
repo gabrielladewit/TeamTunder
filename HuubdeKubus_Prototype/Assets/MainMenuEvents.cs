bd3cd044da66d3f1f4be58a67ac2f1105755e32c
Mike-PC <Mikedhaan@gmail.com>
2018-10-17 15:15:12 +0200

    Intergrated Google Play API
    Need a developer google account to continue working on it

---
+++ HuubdeKubus_Prototype/Assets/MainMenuEvents.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private Text signInButtonText;
+    private Text authStatus;
+
+    public void Start()
+    {
+        signInButtonText = GameObject.Find("SignInButton").GetComponentInChildren<Text>();
+        authStatus = GameObject.Find("authStatus").GetComponent<Text>();
+
+        // Create client configuration
+        PlayGamesClientConfiguration config = new
+            PlayGamesClientConfiguration.Builder()
+            .Build();
+
+        // Enable debugging output (recommended)
+        PlayGamesPlatform.DebugLogEnabled = true;
+
+        // Initialize and activate the platform
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+    }
+
+    public void SignIn()
+    {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            // Sign in with Play Game Services, showing the consent dialog
+            // by setting the second parameter to isSilent=false.
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        }
+        else
+        {
+            // Sign out of play games
+            PlayGamesPlatform.Instance.SignOut();
+
+            // Reset UI
+            signInButtonText.text = "Sign In";
+            authStatus.text = "";
+        }
+    }
+
+    public void SignInCallback(bool success)
+    {
+        if (success)
+        {
+            Debug.Log("(Lollygagger) Signed in!");
+
+            // Change sign-in button text
+            signInButtonText.text = "Sign out";
+
+            // Show the user's name
+            authStatus.text = "Signed in as: " + Social.localUser.userName;
+        }
+        else
+        {
+            Debug.Log("(Lollygagger) Sign-in failed...");
+
+            // Show failure message
+            signInButtonText.text = "Sign in";
+            authStatus.text = "Sign-in failed";
+        }
+    }
+
+}
