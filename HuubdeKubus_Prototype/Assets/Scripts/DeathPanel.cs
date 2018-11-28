d1d30d82108b8876aeceb21d3574fc3c86659cd0
Mike-PC <Mikedhaan@gmail.com>
2018-11-28 14:53:56 +0100

    GameOver UI code done, Visuals not

---
+++ HuubdeKubus_Prototype/Assets/Scripts/DeathPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathPanel : MonoBehaviour {
+
+    public Text progressText;
+    public Text coinText;
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    private void OnEnable()
+    {
+        float progress = GameObject.Find("PlayerSlider").GetComponent<Slider>().value * 100;
+        progressText.text = "" + (int)progress + "%";
+        int coins = 0; // get amount of coins earned in game
+        coinText.text = "" + coins; 
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
