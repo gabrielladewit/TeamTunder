3918b5d25206a1454403ea11e9c5731a4ffce387
Mike den Haan <Mike.den.haan@hva.nl>
2018-12-04 12:08:32 +0100

    Reworked level spawning

--- HuubdeKubus_Prototype/Assets/Scripts/WorldManagers/LoadTables.cs
+++ HuubdeKubus_Prototype/Assets/Scripts/WorldManagers/LoadTables.cs
@@ -2,38 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoadTables : MonoBehaviour {
-
+public class LoadTables : MonoBehaviour
+{
+    Levels levels;
+    Transform worldBlockParent;
     public List<GameObject> tablePrefabs;
     int x = 0, i = 0;
 
     // Use this for initialization
-    void Start () {
-        tablePrefabs = new List<GameObject>(Resources.LoadAll<GameObject>("PrototypePrefabs"));
+    void Start ()
+    {
+        tablePrefabs = new List<GameObject> (Resources.LoadAll<GameObject> ("PrototypePrefabs"));
+        worldBlockParent = GameObject.Find ("GameWorld").transform;
+        levels = GameObject.Find ("UI").GetComponent<Levels> ();
+        LoadLevel (levels.currentLevel);
     }
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update ()
+    {
 		
-	}
+    }
 
-    public GameObject GetRandomTable()
+    public GameObject GetRandomTable ()
     {
         GameObject table;
 
         int x = Random.Range (0, tablePrefabs.Count);
-        table = tablePrefabs[x];
+        table = tablePrefabs [x];
         
         return table;
     }
 
-    public GameObject LoadLevel(int l)
+    public void LoadLevel (int l)
     {
         string levelname = "levels/level" + l;
-        List<GameObject> level = new List<GameObject>(Resources.LoadAll<GameObject>(levelname));
-        GameObject level1 = level[i];
-        i++;
-
-        return level1;
+        List<GameObject> level = new List<GameObject> (Resources.LoadAll<GameObject> (levelname));
+        Debug.Log (level.Count);
+        for (int x = 0; x < level.Count; x++)
+        {
+            if (!level [x].name.Contains ("All"))
+            {
+                GameObject worldBlock = (GameObject)Instantiate (level [x], new Vector3 (0, x * -33f, 0), Quaternion.Euler (-90, 0, 0));
+                worldBlock.transform.parent = worldBlockParent;
+            }
+        }
     }
+        
 }
--- HuubdeKubus_Prototype/Assets/Scripts/WorldManagers/SpawnCollision.cs
+++ HuubdeKubus_Prototype/Assets/Scripts/WorldManagers/SpawnCollision.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class SpawnCollision : MonoBehaviour {
 
-    static GameObject playerObj;
-    static WorldGeneration worldGen;
-    Levels levels;
-    bool triggered = false;
+//    static GameObject playerObj;
+//    static WorldGeneration worldGen;
+//    Levels levels;
+//    bool triggered = false;
 
     // Use this for initialization
     void Start () {
-        levels = GameObject.Find ("UI").GetComponent<Levels> ();
-        playerObj = GameObject.Find("PlayerSphere");
-        worldGen = GameObject.Find ("GameWorld").GetComponent<WorldGeneration>();
+//        levels = GameObject.Find ("UI").GetComponent<Levels> ();
+//        playerObj = GameObject.Find("PlayerSphere");
+//        worldGen = GameObject.Find ("GameWorld").GetComponent<WorldGeneration>();
     }
 
     // Update is called once per frame
@@ -21,13 +21,13 @@ public class SpawnCollision : MonoBehaviour {
 
     }
 
-    private void OnTriggerStay(Collider collision)
-    {
-        if (collision.gameObject == playerObj && !triggered)
-        {
-            triggered = true;
-            worldGen.spawnPuzzle(levels.currentLevel);
-            Destroy(this);
-        }
-    }
+//    private void OnTriggerStay(Collider collision)
+//    {
+//        if (collision.gameObject == playerObj && !triggered)
+//        {
+//            triggered = true;
+//            worldGen.spawnPuzzle(levels.currentLevel);
+//            Destroy(this);
+//        }
+//    }
 }
