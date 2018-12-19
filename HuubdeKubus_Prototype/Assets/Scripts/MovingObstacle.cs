b99426679ca1f1ba853c839dab0f429c40525377
Mike PC <mikedhaan@gmail.com>
2018-12-19 18:01:13 +0100

    SOR Update

--- HuubdeKubus_Prototype/Assets/Scripts/MovingObstacle.cs
+++ HuubdeKubus_Prototype/Assets/Scripts/MovingObstacle.cs
@@ -3,60 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class MovingObstacle : MonoBehaviour {
+public class MovingObstacle : MonoBehaviour
+{
     private List<Vector3> positions = new List<Vector3>();
-    Transform[] places;
+    private Transform[] places;
 
-    Vector3 nextPos;
-    Vector3 lastPos;
-    Vector3 startPos;
+    private Vector3 nextPos;
+    private Vector3 lastPos;
 
-    float distance;
-    int nextLocation = 0;
+    private float distance;
+    private float speed = 15f;
 
-    float speed = 15f;
-    // Use this for initialization
+    private int nextLocation = 0;
+
     void Start()
     {
-        startPos = this.transform.position;
+        //Vul de lijst met plekken van de kinderen van het GameObject.
         places = GetComponentsInChildren<Transform>();
 
         foreach (Transform go in places)
         {
-            if (go.position != startPos && go.name.Contains("GameObject"))
+            //Als je een empty game object toevoegt heet deze altijd GameObject. Er kunnen kinderen op een moving object zitten die geen plek horen te zijn (deze hebben dan ook al vaak een naam).
+            if (go.name.Contains("GameObject"))
+            {
                 positions.Add(go.position);
+            }
         }
-    
+
         nextPos = positions[nextLocation];
     }
-	
-	// Update is called once per frame
-	void Update () {
-        distance = Vector3.Distance(this.transform.position, nextPos);
-        this.transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
 
-        if (distance < 0.75f)
+    void Update()
+    {
+        distance = Vector3.Distance(this.transform.position, nextPos);
+
+        this.transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
+        
+        //Iets voordat het GameObject bij de volgende positie is aangeven dat hij naar de volgende moet.
+        if (distance < 0.5f)
         {
             lastPos = nextPos;
+
             nextLocation++;
+            
             if (nextLocation >= positions.Count)
+            {
                 nextLocation = 0;
+            }
+
             nextPos = positions[nextLocation];
 
-            RotateDieKutAutosGoed();
+            //Rotatie naar het volgende punt toe hoeft maar 1x te gebeuren.
+            RotateObstaclesFacingTarget();
         }
 
     }
 
-    void RotateDieKutAutosGoed()
+    void RotateObstaclesFacingTarget()
     {
         Vector3 targetDir = (nextPos - lastPos).normalized;
         float angle;
 
+        //Onze levels zijn gedraaid op -90 graden. Als een GameObject naar links gaat draait deze dan van 360graden - angle om tot de goede rotatie uit te komen.
         if (targetDir.x < 0)
+        {
             angle = 360 - Vector3.Angle(targetDir, Vector3.up);
+        }
         else
+        {
             angle = Vector3.Angle(targetDir, Vector3.up);
+        }
 
         transform.localRotation = Quaternion.Euler(0, angle, 0);
 
---
+++ HuubdeKubus_Prototype/Assets/Scripts/OldMovingObstacle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OldMovingObstacle : MonoBehaviour {
+
+    Vector3 pos1, pos2, nextPos;
+    public float speed = 15f;
+    float distance;
+
+	// Use this for initialization
+	void Start () {
+        pos1 = this.transform.position;
+        pos2 = this.gameObject.transform.GetChild(0).position;
+        nextPos = pos2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.position = Vector3.MoveTowards(transform.position, nextPos, Time.deltaTime * speed);
+
+        distance = Vector3.Distance(this.transform.position, nextPos);
+
+        if (distance < 0.5f)
+        {
+            if (nextPos == pos1)
+            {
+                nextPos = pos2;
+            }
+            else
+            {
+                nextPos = pos1;
+            }
+
+            transform.LookAt(nextPos);
+        }
+    }
+}
