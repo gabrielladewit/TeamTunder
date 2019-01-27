803bd920990992512bee804cbc2eb7ec06986615
Mike PC <mikedhaan@gmail.com>
2019-01-27 21:31:44 +0100

    Fixes

--- HuubdeKubus_Prototype/Assets/Tween.cs
+++ HuubdeKubus_Prototype/Assets/Tween.cs
@@ -24,7 +24,7 @@ public class Tween : MonoBehaviour
     {
         startTime = Time.time;
         start = transform.position.y;
-        end = start -110;
+        end = start -450;
     }
 
     // Update is called once per frame
